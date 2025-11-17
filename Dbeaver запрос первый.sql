601ce0565cbb9b9e8627c29dc9f4bc348f5a0b0a
Denix1404 <viden2006@gmail.com>
2025-11-17 19:56:11 +0300

    FirstRequests

---
+++ Dbeaver запрос первый.sql
@@ -0,0 +1,16 @@
+SELECT *
+FROM Album
+
+SELECT *
+FROM Album
+WHERE AlbumId = "18"
+
+SELECT *
+FROM Album
+WHERE AlbumId = "4"
+AND ArtistId = "1"
+
+SELECT *
+FROM Album
+WHERE AlbumId = "4"
+OR ArtistId = "2"
